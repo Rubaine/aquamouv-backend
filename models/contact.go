4c6faf84757af91534aebad578e0878dcc196f51
Rubaine <vieira.ruben.sp@gmail.com>
2024-07-22 19:44:55 +0200

    chore: Update database username in .env file and contact model

--- models/contact.go
+++ models/contact.go
@@ -2,8 +2,8 @@ package models
 
 type ContactInfo struct {
 	ID        int    `json:"id" gorm:"primary_key"`
-	FirstName string `json:"name" gorm:"not null"`
-	LastName  string `json:"surname" gorm:"not null"`
+	FirstName string `json:"first_name" gorm:"not null"`
+	LastName  string `json:"last_name" gorm:"not null"`
 	Email     string `json:"email" gorm:"unique;not null"`
 	Phone     string `json:"phone" gorm:"unique;not null"`
 }
