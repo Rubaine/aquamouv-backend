df016533d30c4a820c3b0045708eb749fca85e94
Rubaine <vieira.ruben.sp@gmail.com>
2024-08-11 14:15:42 +0200

    chore: Update contact submission handler to include first and last name in email message

--- handlers/contact/contactHandler.go
+++ handlers/contact/contactHandler.go
@@ -3,30 +3,27 @@ package contact
 import (
 	"remy-aquavelo/config"
 	"remy-aquavelo/models"
-	"time"
 
 	"github.com/kataras/iris/v12"
 )
 
-var c = iris.Cache(10 * time.Second)
-
-func ContactSubmitHandler(ctx iris.Context){
+func ContactSubmitHandler(ctx iris.Context) {
 	var user models.ContactInfo
 
-	if err := ctx.ReadJSON(&user); err != nil{
+	if err := ctx.ReadJSON(&user); err != nil {
 		ctx.StatusCode(iris.StatusBadRequest)
 		ctx.JSON(iris.Map{"error": "request body is not a valid JSON"})
 		return
 	}
 
-	if err := config.Cfg.DB.Create(&user).Error; err != nil{
+	if err := config.Cfg.DB.Create(&user).Error; err != nil {
 		ctx.StatusCode(iris.StatusInternalServerError)
 		ctx.JSON(iris.Map{"error": "failed to store contact information"})
 		return
 	}
 
-	sendMailAsync(user.Email)
-	
+	sendMailAsync(user.Email, user.FirstName, user.LastName)
+
 	ctx.StatusCode(iris.StatusOK)
 	ctx.JSON(iris.Map{"message": "Contact information stored successfully"})
 }
--- handlers/contact/sendMail.go
+++ handlers/contact/sendMail.go
@@ -6,7 +6,7 @@ import (
 	"os"
 )
 
-func sendMailAsync(destinationEmail string) {
+func sendMailAsync(destinationEmail string, firstName string, lastName string) {
 	go func() {
 		auth := smtp.PlainAuth(
 			"",
@@ -15,7 +15,24 @@ func sendMailAsync(destinationEmail string) {
 			os.Getenv("MAIL_HOST"),
 		)
 
-		msg := "Subject: Contact Information\n\nThank you for contacting us. We will get back to you as soon as possible."
+		msg := fmt.Sprintf(`Subject: Contact Information Aquamouv
+
+Bonjour %s %s,
+
+Nous avons bien reçu votre demande pour une séance découverte gratuite. Le centre Aquavelo de Senlis est heureux de vous accueillir pour 
+vous faire essayer un cours d'aquabiking coaché par des professeurs de sport diplômés à la suite d'un bilan durant lequel nous définirons 
+vos besoins et vos objectifs. 
+Prenez vite rendez-vous avec le centre Aquavelo de Senlis en appelant au 06 20 52 17 37. Pour effectuer votre séance, pensez à prendre un 
+maillot de bain, une serviette de bain, un gel douche, une bouteille d'eau et des chaussures d'aquavélo.
+
+Horaires d'ouverture: Du Lundi au vendredi 9h-20h / Samedi 9h-16h / Dimanche 9h-15h / Jours Fériés horaires du week-end 
+Adresse: 60300 Senlis, France
+
+*Offre non cumulable.
+
+Cordialement,
+L'équipe Aquavelo`, lastName, firstName)
+
 		err := smtp.SendMail(
 			os.Getenv("MAIL_HOST")+":"+os.Getenv("MAIL_PORT"),
 			auth,
