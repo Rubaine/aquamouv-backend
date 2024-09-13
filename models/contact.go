64c119fb3008eef3a14ba45f9f1c51fa7a3f1cc5
Ruben Vieira <vieira.ruben.sp@gmail.com>
2024-09-13 19:20:10 +0300

    chore: Update contact model to include created_at field

--- models/contact.go
+++ models/contact.go
@@ -1,9 +1,12 @@
 package models
 
+import "time"
+
 type ContactInfo struct {
-	ID        int    `json:"id" gorm:"primary_key"`
-	FirstName string `json:"first_name" gorm:"not null"`
-	LastName  string `json:"last_name" gorm:"not null"`
-	Email     string `json:"email" gorm:"not null"`
-	Phone     string `json:"phone" gorm:"not null"`
+	ID        int       `json:"id" gorm:"primary_key"`
+	FirstName string    `json:"first_name" gorm:"not null"`
+	LastName  string    `json:"last_name" gorm:"not null"`
+	Email     string    `json:"email" gorm:"not null"`
+	Phone     string    `json:"phone" gorm:"not null"`
+	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 }
