d4ffe0487a6e7d1ae42b501ba99ba0d6deeff727
Ruben Vieira <vieira.ruben.sp@gmail.com>
2024-09-13 19:14:54 +0300

    chore: Update contact model to remove unique constraint on email and phone fields

--- models/contact.go
+++ models/contact.go
@@ -4,6 +4,6 @@ type ContactInfo struct {
 	ID        int    `json:"id" gorm:"primary_key"`
 	FirstName string `json:"first_name" gorm:"not null"`
 	LastName  string `json:"last_name" gorm:"not null"`
-	Email     string `json:"email" gorm:"unique;not null"`
-	Phone     string `json:"phone" gorm:"unique;not null"`
+	Email     string `json:"email" gorm:"not null"`
+	Phone     string `json:"phone" gorm:"not null"`
 }
